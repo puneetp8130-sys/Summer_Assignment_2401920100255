b3e35e93d72ff387ce25f35ac1dfd6f46791147f
puneetp8130-sys <puneetp8130@gmail.com>
2026-06-07 14:22:10 +0530

    Added First Unique Character in a String Solution

---
+++ Week_2/Day_1/First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>mpp(26,0);
+
+        for(char c : s){
+            mpp[c-'a']++;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(mpp[s[i]-'a'] == 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
